a2e6c2a0bc852a5aa2a111e158f384a514f2290f
Madlena Ivanova <madlena.iv@abv.bg>
2022-11-20 11:39:07 +0200

    Add project files.

---
+++ SpookyManors/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SpookyManors.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
