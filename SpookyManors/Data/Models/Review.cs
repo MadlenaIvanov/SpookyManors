03676e81c5487f1b7c971d2455e64ab6e9890b3a
Madlena Ivanova <madlena.iv@abv.bg>
2022-11-24 22:44:49 +0200

    added review

---
+++ SpookyManors/Data/Models/Review.cs
@@ -0,0 +1,11 @@
+﻿namespace SpookyManors.Data.Models
+{
+    public class Review
+    {
+        public int Id { get; init; }
+
+        public string Name { get; set; }
+
+        public string Comment { get; set; }
+    }
+}
