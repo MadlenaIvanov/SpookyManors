db76917e5d315816c28b642474202bab601ebc5f
Madlena Ivanova <madlena.iv@abv.bg>
2022-12-09 22:08:02 +0200

    added class case

---
+++ SpookyManors/Data/Models/Case.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SpookyManors.Data.Models
+{
+    public class Case
+    {
+        public int Id { get; init; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string ImageUrl { get; set; }
+
+
+        public int CategoryId { get; set; }
+
+        public Category Category { get; init; }
+
+        public DateTime DateTime { get; set; }
+    }
+}
