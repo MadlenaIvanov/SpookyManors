9ed47bd6fb6e75e229ef6a011b01926b9c1d9b9f
Madlena Ivanova <madlena.iv@abv.bg>
2022-12-11 21:55:50 +0200

    adding class history

---
+++ SpookyManors/Data/Models/History.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SpookyManors.Data.Models
+{
+    public class History
+    {
+        public int Id { get; init; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        public string About { get; set; }
+
+        [Required]
+        public string ImageUrl { get; set; }
+    }
+}
